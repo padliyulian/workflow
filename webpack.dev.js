95899ac84e7f27f963b82da53e96f87e4f48a29f
Padli Yulian <padliyulian@ymail.com>
2019-06-16 21:41:26 +0700

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,8 @@ module.exports = merge(common, {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html"
+      template: "./src/template.html",
+      favicon: "./src/assets/img/favicon.png"
     })
   ],
   module: {
