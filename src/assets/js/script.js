5e33a36a4057e5e74c6e45d12b86cbe6aedb50f0
Padli Yulian <padliyulian@ymail.com>
2019-07-16 13:30:11 +0700

    refactor

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,7 @@
 document.querySelector('body,html').addEventListener('click', () => {
   console.log('ok');
 });
+
+// test babel preset
+const julianHello = (name) => console.log(`hello ${name}`);
+julianHello('Julian');
